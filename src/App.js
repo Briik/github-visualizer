62b6478ce2e7f550cc34691f77e5813689a77f25
Briik <bryce.lively@gmail.com>
2018-01-06 12:54:40 -0500

    transform-class-properties for fat arrow autobinding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { PureComponent } from 'react';
 
 export default class App extends PureComponent {
-  render() {
-    return <h1>Hello World</h1>
-  }
+  render = () => <h1>Hello World</h1>
 }
